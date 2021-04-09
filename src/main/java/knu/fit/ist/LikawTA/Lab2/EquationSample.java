856ca642c3b4495f66a71ef43e0c6925f8b0531e
likawka <68962708+likawka@users.noreply.github.com>
2021-04-09 13:39:47 +0300

    Add test-test, lab 2 page and EquationSample

---
+++ src/main/java/knu/fit/ist/LikawTA/Lab2/EquationSample.java
@@ -0,0 +1,20 @@
+package knu.fit.ist.LikawTA.Lab2;
+
+import java.text.DecimalFormat;
+import static java.lang.Math.PI;
+
+
+public class EquationSample {
+ public static void main(String[] args){
+
+//     DecimalFormat df = new DecimalFormat("###.##");
+//     System.out.println(df.format(PI));
+
+      float f1 = 2.035678f;
+      float f2 = 2.056587f;
+
+     System.out.println(Math.abs(f1-f2)<=0.01f);
+
+ }
+
+}
--- src/test/java/knu/fit/ist/LikawTA/LikawTaApplicationTests.java
+++ src/test/java/knu/fit/ist/LikawTA/LikawTaApplicationTests.java
@@ -1,5 +1,6 @@
 package knu.fit.ist.LikawTA;
 
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
