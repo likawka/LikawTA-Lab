cdf3ede88ec8e521df8e1be097734a2505b7b1ad
likawka <68962708+likawka@users.noreply.github.com>
2021-04-16 12:57:34 +0300

    add test to lab3 with array

---
+++ src/test/java/knu/fit/ist/LikawTA/Lab3Tests.java
@@ -0,0 +1,20 @@
+package knu.fit.ist.LikawTA;
+
+
+
+import knu.fit.ist.LikawTA.Lab3.ArraySqrt;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+
+public class Lab3Tests {
+
+    @Test
+    void ArrayTest() {
+        ArraySqrt calc = new ArraySqrt();
+        int[]a ={3,5,4,25};
+        assertEquals(calc.squareNumber(a, 2), 2);
+    }}
+
