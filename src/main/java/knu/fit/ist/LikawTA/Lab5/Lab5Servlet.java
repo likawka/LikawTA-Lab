9df28eca54c7919ccbd647c542c9ead52bbf7379
likawka <68962708+likawka@users.noreply.github.com>
2021-05-20 23:14:20 +0300

    lab 5 new

--- src/main/java/knu/fit/ist/LikawTA/Lab5/Lab5Servlet.java
+++ src/main/java/knu/fit/ist/LikawTA/Lab5/Lab5Servlet.java
@@ -11,10 +11,6 @@ import javax.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.context.support.SpringBeanAutowiringSupport;
 
-/**
- *
- * @author skinf
- */
 @WebServlet(name = "Lab5Servlet", urlPatterns = {"/lab5"})
 public class Lab5Servlet extends HttpServlet {
 
@@ -30,15 +26,6 @@ public class Lab5Servlet extends HttpServlet {
                 config.getServletContext());
     }
 
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
@@ -56,15 +43,6 @@ public class Lab5Servlet extends HttpServlet {
         }
     }
 
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /**
-     * Handles the HTTP <code>GET</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
@@ -72,14 +50,6 @@ public class Lab5Servlet extends HttpServlet {
 
     }
 
-    /**
-     * Handles the HTTP <code>POST</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
@@ -102,12 +72,6 @@ public class Lab5Servlet extends HttpServlet {
         request.getRequestDispatcher("lab5.jsp").forward(request, response);
 
     }
-
-    /**
-     * Returns a short description of the servlet.
-     *
-     * @return a String containing servlet description
-     */
     @Override
     public String getServletInfo() {
         return "Short description";
